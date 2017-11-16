9f0faf75266505443b296c18d245513cfe0d83f5
steven <1652080698@qq.com>
2017-11-16 17:30:08 +0800

    chat initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# go-realtimechat
+go 聊天服务
+
+## 运行
+go build ./<br>
+./go-reltimechat
---
+++ main.go
@@ -0,0 +1,134 @@
+package main
+
+import (
+	"github.com/gorilla/websocket"
+	"net/http"
+	"log"
+	"fmt"
+	"encoding/json"
+	//"strings"
+	"strings"
+)
+
+var (
+	// websocket upgrader
+	upgrader = websocket.Upgrader{}
+	// chat
+	chat = NewChat()
+)
+
+//初始化配置
+func init()  {
+	initConfig();
+}
+
+func main() {
+
+	// chat html
+	fs := http.FileServer(http.Dir("public"))
+	http.Handle("/", fs)
+	// user list
+	http.HandleFunc("/users", userList)
+	// group list
+	http.HandleFunc("/groups", groupList)
+	// handle websocket
+	http.HandleFunc("/ws", handleConnections)
+
+	// start read message
+	go handleMessages()
+
+	log.Println("http server started on : 8087")
+	err := http.ListenAndServe(":8087", nil)
+	if(err != nil) {
+		log.Println("http server error: "+ err.Error())
+	}
+}
+
+// all user
+//return json
+func userList(w http.ResponseWriter, req *http.Request)  {
+	users := chat.Users()
+	v, _ := json.Marshal(users)
+	w.Write(v)
+}
+
+// all group
+// return json
+func groupList(w http.ResponseWriter, req *http.Request)  {
+	groups := chat.Groups()
+	v, _ := json.Marshal(groups)
+	w.Write(v)
+}
+
+
+// handle ws conn
+func handleConnections(w http.ResponseWriter, req *http.Request) {
+
+	userId := req.FormValue("user_id")
+	roomId := req.FormValue("room_id")
+	if(userId == "") {
+		return
+	}
+	if(roomId == "") {
+		return
+	}
+	log.Println("room_id : "+ roomId +" user_id: "+ userId +" come in!")
+
+	//将 get 请求升级为 websocket
+	ws, err := upgrader.Upgrade(w, req, nil)
+	if(err != nil) {
+		fmt.Println("请求出错:" + err.Error())
+		return
+	}
+	defer ws.Close()
+
+	// add group
+	chat.AddGroup(roomId, "default_"+roomId)
+	// add user
+	chat.AddUser(userId, roomId, "user_"+userId, ws)
+
+	for {
+		// read ws json data
+		messageType, p, error := ws.ReadMessage()
+		message := strings.Replace(string(p), "\n", "", -1)
+		if error != nil {
+			log.Println(err.Error())
+			log.Println("room_id : "+ roomId +" user_id: "+ userId +" exit!")
+			chat.DeleteUser(userId)
+			break
+		} else {
+			log.Println(messageType)
+			log.Println(message)
+		}
+		var messageMap Messages
+		json.Unmarshal([]byte(message), &messageMap)
+		msg := Messages {
+			Type: messageMap.Type,
+			Message: messageMap.Message,
+			UserId: messageMap.UserId,
+		}
+
+		// send message to messageChan
+		chat.MessageChan <- msg
+	}
+}
+
+// handle read message
+func handleMessages()  {
+	for {
+		// read message from chat messageChan
+		var msg Messages
+		msg = <- chat.MessageChan
+
+		// 暂时先广播给所有房间的所有人
+		for _, user := range chat.Users() {
+			ws := user.Ws
+			err := ws.WriteJSON(msg)
+			if(err != nil) {
+				chat.DeleteUser(user.Id)
+				log.Printf("error: %v", err)
+			}
+		}
+	}
+}
+
